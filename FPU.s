ae7e7dfe10b78bf31ffe4a00d40c2fc2b4e9b9eb
Khanjan14 <khanjandesai59@gmail.com>
2017-12-01 21:06:01 +0530

    Create FPU.s

---
+++ FPU.s
@@ -0,0 +1,25 @@
+    PRESERVE8
+    AREA     Floating_point, CODE, READONLY
+    EXPORT __main
+	ENTRY	
+__main  FUNCTION
+		mov r0,#0x3f800000
+		mov r1,#0x3f800000
+		mov r5,#0x3fc00000     ; Assigning value of x = .25
+		mov r6,#0	  ; Count
+		vmov.f32 s0,r0
+		vmov.f32 s1,r1
+		vmov.f32 s2,s1
+		vmov.f32 s5,r5
+		vmov.f32 s6,r6
+		
+loop	adds r6,r6,#1
+		vmul.f32 s1,s1,s5
+		vdiv.f32 s1,s1,s6
+		vadd.f32 s0,s0,s1
+		cmp r6,#8
+		bne loop
+
+stop B stop
+	ENDFUNC
+	END
